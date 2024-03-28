e3d19f0b9320e9f68902ceddadaf1d8a62b0d87d
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2024-03-28 13:27:52 +0530

    Fix: readMe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-moneyhub-api-client node library integration to: 
-query for Account Balance and Transactions after getting consent from the bank account holder
+moneyhub-api-client node library integration to Query for Account Balance and Transactions after getting consent from the Bank Account Holder
+
+Steps to run the Project
+1. Clone the Repository
+2. npm i
+3. Add client details in config.js file present in the root folder
+4. node app.js command will start listening on Port 3000
---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+  resourceServerUrl: "https://api.moneyhub.co.uk/v2.0",
+  identityServiceUrl: "https://identity.moneyhub.co.uk/oidc",
+  accountConnectUrl: "https://bank-chooser.moneyhub.co.uk/account-connect.js",
+  client: {
+    client_id: "",
+    client_secret: "",
+    token_endpoint_auth_method: "",
+    id_token_signed_response_alg: "",
+    request_object_signing_alg: "",
+    redirect_uri: "",
+    response_type: "",
+    keys: [],
+  },
+};
+
+module.exports = config;
